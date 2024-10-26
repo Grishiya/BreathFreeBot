ab73177bda3465c4b7c1b608461d7a6b400c5211
guruw <guruwolf8@gmail.com>
2024-10-26 12:40:09 +0300

    Создание бота для людей которые хотят бросить курить

---
+++ src/main/java/com/grishiya/breathfree/BreathFreeBot/BreathFreeBotApplication.java
@@ -0,0 +1,13 @@
+package com.grishiya.breathfree.BreathFreeBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreathFreeBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreathFreeBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grishiya/breathfree/BreathFreeBot/BreathFreeBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grishiya.breathfree.BreathFreeBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreathFreeBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
